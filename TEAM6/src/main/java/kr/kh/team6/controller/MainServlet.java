2997ec63aedd6e9918634dc3abb8825660ba311b
jiyong143 <153248126+jiyong143@users.noreply.github.com>
2024-02-28 17:06:42 +0900

    ㄱㄱ

--- TEAM6/src/main/java/kr/kh/team6/controller/MainServlet.java
+++ TEAM6/src/main/java/kr/kh/team6/controller/MainServlet.java
@@ -12,21 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public MainServlet() {
-        
-    }
-
+   
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String data = request.getParameter("data");
-		System.out.println("전송 받은 데이터 : " + data);
-		String id = request.getParameter("id");
-		System.out.println("전송 받은 ID : " + id);
 		
-		//화면으로 나이를 전송
-		int age = 30;
-		request.setAttribute("age", age);//서버에서 화면으로 age를 전송
 		request.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(request, response);
 	}
 
--- src/main/java/kr/kh/team6/model/vo/MemberVO.java
+++ src/main/java/kr/kh/team6/model/vo/MemberVO.java
@@ -1,6 +1,10 @@
 package kr.kh.team6.model.vo;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class MemberVO {
 	private String me_id;
 	private int me_birth;
@@ -8,7 +12,7 @@ public class MemberVO {
 	private String me_phone;
 	private String me_pw;
 	private String me_address;
-	
-	
-
+	private String me_name;
+	private String me_ms_state;
+	private String me_authority;
 }
