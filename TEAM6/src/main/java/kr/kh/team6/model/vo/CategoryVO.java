ed8ad4f3508237593b1c1b7b10405c826f627007
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-03-03 23:57:04 +0900

    정경호 - 구현중

--- TEAM6/src/main/java/kr/kh/team6/model/vo/CategoryVO.java
+++ TEAM6/src/main/java/kr/kh/team6/model/vo/CategoryVO.java
@@ -6,8 +6,15 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class CategoryVO {
-	
 	private int ca_num;
 	private String ca_title;
+	
+	
+	
+	public CategoryVO(int ca_num, String ca_title) {
+		super();
+		this.ca_num = ca_num;
+		this.ca_title = ca_title;
+	}
 
 }
