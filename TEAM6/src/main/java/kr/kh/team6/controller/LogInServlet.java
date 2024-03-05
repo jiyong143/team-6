4623fca13d37c53404df596b08c54281f03abe42
Tomi606 <001dosirak@naver.com>
2024-03-05 18:25:03 +0900

    로그인서블렛

--- TEAM6/src/main/java/kr/kh/team6/controller/LogInServlet.java
+++ TEAM6/src/main/java/kr/kh/team6/controller/LogInServlet.java
@@ -14,30 +14,39 @@ import kr.kh.team6.model.vo.MemberVO;
 import kr.kh.team6.service.MemberService;
 import kr.kh.team6.service.MemberServiceImp;
 
+
 @WebServlet("/login")
 public class LogInServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private MemberService memberService = new MemberServiceImp();
+    private MemberService memberService = new MemberServiceImp();
+   
+    public LogInServlet() {
+        
+    }
 
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
 	}
 
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		String id = request.getParameter("id");
-		String pw = request.getParameter("pw");
-
-		MemberVO user = memberService.login(new LoginDTO(id, pw));
-		
-		if(user != null) {
+		String pw = request.getParameter("pw"); 
+		MemberVO user = memberService.getMember(new LoginDTO(id,pw));
+		if(user!=null) {
+			request.setAttribute("msg", "로그인에 성공했습니다.");
+			request.setAttribute("url","");
+			// 세션에 로그인한 회원의 정보를 저장해서 유지시킨다
 			HttpSession session = request.getSession();
 			session.setAttribute("user", user);
-			response.sendRedirect(request.getContextPath() + "/");
+		}else {
+			// 실패하면 다시 로그인 페이지로 이동
+			request.setAttribute("msg","로그인에 실패했습니다.");
+			request.setAttribute("url","login");	
 		}
-		else {
-			doGet(request, response);
-		}
+		request.getRequestDispatcher("/WEB-INF/views/message.jsp").forward(request, response);
+		
 	}
 
 }
