6f8e0cd3d308ccd5317b4212a53faeea8b460420
jiyong143 <153248126+jiyong143@users.noreply.github.com>
2024-03-20 17:39:26 +0900

    댓글 수정 화면에 토글 문제 해결

--- TEAM6/src/main/java/kr/kh/team6/controller/CommentUpdateServlet.java
+++ TEAM6/src/main/java/kr/kh/team6/controller/CommentUpdateServlet.java
@@ -1,6 +1,10 @@
 package kr.kh.team6.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,9 +12,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.kh.team6.model.vo.BoardVO;
+import kr.kh.team6.model.vo.CategoryVO;
 import kr.kh.team6.model.vo.CommentVO;
 import kr.kh.team6.model.vo.MemberVO;
 import kr.kh.team6.model.vo.PostVO;
+import kr.kh.team6.service.BoardService;
+import kr.kh.team6.service.BoardServiceImp;
+import kr.kh.team6.service.CategoryService;
+import kr.kh.team6.service.CategoryServiceImp;
 import kr.kh.team6.service.CommentService;
 import kr.kh.team6.service.CommentServiceImp;
 import kr.kh.team6.service.MemberService;
@@ -25,15 +35,35 @@ public class CommentUpdateServlet extends HttpServlet {
     private CommentService commentService = new CommentServiceImp();
     private PostService postService = new PostServiceImp();
     private MemberService memberService = new MemberServiceImp();
+    private BoardService boardService = new BoardServiceImp();
+	private CategoryService categoryService = new CategoryServiceImp();
+    
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		// 왼쪽 토글 : 게시판들을 모두 가져온다
+		ArrayList<BoardVO> bList = boardService.getBoardList();
+		// 게시판을 포함하는 카테고리들의 번호로 구성된 리스트를 만든다
+		List<Integer> list = new ArrayList<Integer>();
+		for (BoardVO board : bList) {
+			list.add(board.getBo_ca_num());
+		}
+
+		// 위 리스트 중복 제거
+		Set<Integer> set = new HashSet<>(list);
+		List<Integer> cNumList = new ArrayList<>(set);
+		// 게시판 갖는 카테고리 리스트 생성
+		ArrayList<CategoryVO> categoryList = new ArrayList<CategoryVO>();
+		for (Integer cNum : cNumList) {
+			categoryList.add(categoryService.getCategory(cNum));
+		}
+		// 검색어를 주고 게시글 리스트를 가져옴
+		request.setAttribute("categoryList", categoryList);
+	
 		String bName = request.getParameter("bName");
 		int bNum = Integer.parseInt(request.getParameter("bNum"));
 		request.setAttribute("bName", bName);
 		request.setAttribute("bNum", bNum);
-		
-		
 		int cNum;
 		try {
 			cNum = Integer.parseInt(request.getParameter("cNum"));
