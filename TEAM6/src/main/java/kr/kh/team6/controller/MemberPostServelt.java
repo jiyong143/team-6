b9db371ba16a8683dc4afb3b59b4e16ff551ddce
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-03-22 16:32:42 +0900

    수정

---
+++ TEAM6/src/main/java/kr/kh/team6/controller/MemberPostServelt.java
@@ -0,0 +1,48 @@
+package kr.kh.team6.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.kh.team6.model.vo.BoardVO;
+import kr.kh.team6.model.vo.MemberVO;
+import kr.kh.team6.model.vo.PostVO;
+import kr.kh.team6.service.PostService;
+import kr.kh.team6.service.PostServiceImp;
+
+@WebServlet("/member/post")
+public class MemberPostServelt extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private PostService postService = new PostServiceImp();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+
+		MemberVO user = (MemberVO) request.getSession().getAttribute("user");
+		if (user == null) {
+			request.setAttribute("msg", "로그인이 필요한 서비스입니다.");
+			request.setAttribute("url", "login");
+			request.getRequestDispatcher("/WEB-INF/views/message.jsp").forward(request, response);
+		}
+		
+		int po_bo_num;
+		try {
+			po_bo_num = Integer.parseInt(request.getParameter("po_bo_num"));
+		} catch (Exception e) {
+			po_bo_num = 0;
+		}
+		ArrayList<BoardVO> board = postService.selectBoard(po_bo_num);
+		ArrayList<PostVO> postList = postService.getAllPostList();
+		
+		
+		
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+
+}
