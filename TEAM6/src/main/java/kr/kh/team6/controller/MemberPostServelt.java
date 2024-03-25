77ddbfdf42883835e5dadce37162b6aae1c9a248
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-03-25 10:52:43 +0900

    내 게시글 조회할때 토글 뜨게 함

--- TEAM6/src/main/java/kr/kh/team6/controller/MemberPostServelt.java
+++ TEAM6/src/main/java/kr/kh/team6/controller/MemberPostServelt.java
@@ -2,6 +2,9 @@ package kr.kh.team6.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,17 +13,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.kh.team6.model.vo.BoardVO;
+import kr.kh.team6.model.vo.CategoryVO;
+import kr.kh.team6.model.vo.CommentVO;
 import kr.kh.team6.model.vo.MemberVO;
 import kr.kh.team6.model.vo.PostVO;
+import kr.kh.team6.service.BoardService;
+import kr.kh.team6.service.BoardServiceImp;
+import kr.kh.team6.service.CategoryService;
+import kr.kh.team6.service.CategoryServiceImp;
 import kr.kh.team6.service.PostService;
 import kr.kh.team6.service.PostServiceImp;
 
 @WebServlet("/member/post")
 public class MemberPostServelt extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    private PostService postService = new PostServiceImp();
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	private PostService postService = new PostServiceImp();
+	private BoardService boardService = new BoardServiceImp();
+	private CategoryService categoryService = new CategoryServiceImp();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		MemberVO user = (MemberVO) request.getSession().getAttribute("user");
 		if (user == null) {
@@ -28,8 +40,20 @@ public class MemberPostServelt extends HttpServlet {
 			request.setAttribute("url", "login");
 			request.getRequestDispatcher("/WEB-INF/views/message.jsp").forward(request, response);
 		}
-		
-		int po_bo_num,po_num;
+		ArrayList<BoardVO> bList = boardService.getBoardList();
+		List<Integer> list = new ArrayList<Integer>();
+		for (BoardVO board : bList) {
+			list.add(board.getBo_ca_num());
+		}
+		Set<Integer> set = new HashSet<>(list);
+		List<Integer> cNumList = new ArrayList<>(set);
+		ArrayList<CategoryVO> categoryList = new ArrayList<CategoryVO>();
+		for (Integer cNum : cNumList) {
+			categoryList.add(categoryService.getCategory(cNum));
+		}
+		request.setAttribute("categoryList", categoryList);
+
+		int po_bo_num, po_num;
 		try {
 			po_bo_num = Integer.parseInt(request.getParameter("po_bo_num"));
 			po_num = Integer.parseInt(request.getParameter("po_num"));
@@ -37,16 +61,11 @@ public class MemberPostServelt extends HttpServlet {
 			po_bo_num = 0;
 			po_num = 0;
 		}
-		
-//		ArrayList<BoardVO> board = postService.selectBoard(po_bo_num);
 		ArrayList<PostVO> postList = postService.getAllPostList();
-		
+
 		request.setAttribute("postList", postList);
 		request.getRequestDispatcher("/WEB-INF/views/member/post.jsp").forward(request, response);
-		
-		
-		
-	}
 
+	}
 
 }
